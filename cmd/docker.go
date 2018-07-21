8961a08bc951b42e13e75604089be0d24d7a7046
Adam Houston <phazyy9@gmail.com>
2018-07-21 22:20:55 +0100

    #18 : Implementing Docker client creation and build execution

--- cmd/docker.go
+++ cmd/docker.go
@@ -2,13 +2,15 @@ package cmd
 
 import (
 	"archive/tar"
-	"bytes"
+	"context"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/urfave/cli"
 )
 
@@ -20,48 +22,79 @@ var Docker = cli.Command{
 	Action:  DockerAction,
 }
 
+const (
+	apiSocket  = "unix:///var/run/docker.sock"
+	apiVersion = "v1.24"
+)
+
+var (
+	apiHeaders = map[string]string{"User-Agent": "engine-api-cli-1.0"}
+)
+
 // DockerAction handles building a container
 func DockerAction(c *cli.Context) error {
-	fmt.Println("Docker")
 	// Create types.ImageBuildOptions from config and flags
 	// Compress directory including Dockerfile into tar
 	// Send tar as Docker Context using ImageBuild()
-	err := CreateArchive(".")
+	ctx := context.Background()
+	reader, writer := io.Pipe()
+
+	go func() {
+		err := CreateArchive(".", writer)
+		if err != nil {
+			panic(err)
+		}
+		writer.Close()
+	}()
+
+	opts := types.ImageBuildOptions{
+		Tags:       []string{"atlas-dockerfile:test"},
+		Dockerfile: "./Dockerfile",
+		BuildArgs:  map[string]*string(nil),
+	}
+
+	cli, err := client.NewClient(apiSocket, apiVersion, nil, apiHeaders)
+	res, err := cli.ImageBuild(ctx, reader, opts)
 	if err != nil {
 		panic(err)
 	}
+
+	defer res.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bodyBytes))
+
 	return nil
 }
 
 // CreateArchive creates a .tar archive to be passed into Docker's ImageBuild()
-func CreateArchive(p string) error {
-	buffer := bytes.NewBuffer(nil)
-	tw := tar.NewWriter(buffer)
+func CreateArchive(p string, w io.Writer) error {
+	tw := tar.NewWriter(w)
 	defer tw.Close()
 
 	return filepath.Walk(p, func(file string, fi os.FileInfo, err error) error {
-		if err != nil {
+		if err != nil || !fi.Mode().IsRegular() {
 			return err
 		}
 
-		header, err := tar.FileInfoHeader(fi, fi.Name())
+		fmt.Println(file)
+		header, err := tar.FileInfoHeader(fi, file)
 		if err != nil {
 			return err
 		}
-		header.Name = strings.TrimPrefix(strings.Replace(file, p, "", -1), string(filepath.Separator))
+		header.Name = file
 		if err := tw.WriteHeader(header); err != nil {
 			return err
 		}
 
-		if !fi.Mode().IsRegular() {
-			return nil
-		}
-
 		f, err := os.Open(file)
-		defer f.Close()
 		if err != nil {
 			return err
 		}
+		defer f.Close()
 
 		if _, err := io.Copy(tw, f); err != nil {
 			return err
