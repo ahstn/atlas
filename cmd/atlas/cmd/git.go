7332314fc53610ed1242e47c42e7f398a4073dfb
Adam Houston <phazyy9@gmail.com>
2018-08-16 21:36:26 +0100

    #12: General clean up

--- cmd/atlas/cmd/git.go
+++ cmd/atlas/cmd/git.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"path"
 
 	"github.com/ahstn/atlas/cmd/atlas/flag"
@@ -21,44 +22,55 @@ const (
 	logUpdating  = "\n:arrows_clockwise:Updating [%s]..."
 )
 
-// Git defines the command for the preforming git actions against services
-// i.e atlas git checkout master -e app1
-var Git = cli.Command{
-	Name:    "git",
-	Aliases: []string{"g"},
-	Usage:   "preform Git actions against service(s)",
-	Subcommands: []cli.Command{
-		{
-			Name:      "clone",
-			Usage:     "clone the services' repo(s) defined in config",
-			ArgsUsage: "[single service]",
-			Action:    cloneAction,
-			Flags:     []cli.Flag{flag.Config, flag.Exclude},
+var (
+	// Git defines the command for the preforming git actions against services
+	// i.e atlas git checkout master -e app1
+	Git = cli.Command{
+		Name:    "git",
+		Aliases: []string{"g"},
+		Usage:   "preform Git actions against service(s)",
+		Subcommands: []cli.Command{
+			branch,
+			clone,
+			checkout,
+			update,
 		},
-		{
-			Name:      "branch",
-			Usage:     "create a branch in the services' repo(s) defined in config",
-			ArgsUsage: "[branch name]",
-			Action:    branchAction,
-			Flags:     []cli.Flag{flag.Config, flag.Exclude},
-		},
-		{
-			Name:      "checkout",
-			Usage:     "checkout a branch in services' repo(s) defined in config",
-			ArgsUsage: "[branch]",
-			Action:    checkoutAction,
-			Flags:     []cli.Flag{flag.Config, flag.Exclude},
-		},
-		{
-			Name:    "update",
-			Aliases: []string{"up"},
-			Usage:   "pull updates from remote, but keep local changes",
-			Flags:   []cli.Flag{flag.Config, flag.Exclude},
-		},
-	},
-}
+	}
 
-func cloneAction(c *cli.Context) error {
+	clone = cli.Command{
+		Name:      "clone",
+		Usage:     "clone the services' repo(s) defined in config",
+		ArgsUsage: "[single service]",
+		Action:    gitAction,
+		Flags:     []cli.Flag{flag.Config, flag.Exclude},
+	}
+
+	branch = cli.Command{
+		Name:      "branch",
+		Usage:     "create a branch in the services' repo(s) defined in config",
+		ArgsUsage: "[branch name]",
+		Action:    gitAction,
+		Flags:     []cli.Flag{flag.Config, flag.Exclude},
+	}
+
+	checkout = cli.Command{
+		Name:      "checkout",
+		Usage:     "checkout a branch in services' repo(s) defined in config",
+		ArgsUsage: "[branch]",
+		Action:    gitAction,
+		Flags:     []cli.Flag{flag.Config, flag.Exclude},
+	}
+
+	update = cli.Command{
+		Name:    "update",
+		Aliases: []string{"up"},
+		Usage:   "pull updates from remote, but keep local changes",
+		Action:  gitAction,
+		Flags:   []cli.Flag{flag.Config, flag.Exclude},
+	}
+)
+
+func gitAction(c *cli.Context) error {
 	f, err := validator.ValidateExists(c.String("config"))
 	if err != nil {
 		panic(err)
@@ -76,86 +88,23 @@ func cloneAction(c *cli.Context) error {
 			continue
 		}
 
-		emoji.Printf(logClone, app.Repo, app.Name)
-		_, err := git.Clone(cfg.Root, app.Repo, app.Name)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	return nil
-}
-
-func checkoutAction(c *cli.Context) error {
-	f, err := validator.ValidateExists(c.String("config"))
-	if err != nil {
-		panic(err)
-	}
-
-	cfg, err := config.Read(f)
-	if err != nil {
-		panic(err)
-	}
-
-	emoji.Printf(logOperating, cfg.Root)
-	for _, app := range cfg.Services {
-		if util.StringSliceContains(c.StringSlice("exclude"), app.Name) {
-			emoji.Printf(logSkipping, app.Name)
-			continue
+		var out []byte
+		switch c.Command.Name {
+		case "clone":
+			emoji.Printf(logClone, app.Repo, app.Name)
+			out, _ = git.Clone(cfg.Root, app.Repo, app.Name)
+		case "checkout":
+			emoji.Printf(logCheckout, c.Args().First(), app.Name)
+			out, _ = git.CheckoutBranch(path.Join(cfg.Root, app.Name), c.Args().First())
+		case "branch":
+			emoji.Printf(logNewBranch, c.Args().First(), app.Name)
+			out, _ = git.CreateBranch(path.Join(cfg.Root, app.Name), c.Args().First())
+		case "update":
+			emoji.Printf(logUpdating, app.Name)
+			out, _ = git.Update(path.Join(cfg.Root, app.Name))
 		}
 
-		emoji.Printf(logCheckout, c.Args().First(), app.Name)
-		git.CheckoutBranch(path.Join(cfg.Root, app.Name), c.Args().First())
-	}
-
-	return nil
-}
-
-func branchAction(c *cli.Context) error {
-	f, err := validator.ValidateExists(c.String("config"))
-	if err != nil {
-		panic(err)
-	}
-
-	cfg, err := config.Read(f)
-	if err != nil {
-		panic(err)
-	}
-
-	emoji.Printf(logOperating, cfg.Root)
-	for _, app := range cfg.Services {
-		if util.StringSliceContains(c.StringSlice("exclude"), app.Name) {
-			emoji.Printf(logSkipping, app.Name)
-			continue
-		}
-
-		emoji.Printf(logNewBranch, c.Args().First(), app.Name)
-		git.CreateBranch(path.Join(cfg.Root, app.Name), c.Args().First())
-	}
-
-	return nil
-}
-
-func updateAction(c *cli.Context) error {
-	f, err := validator.ValidateExists(c.String("config"))
-	if err != nil {
-		panic(err)
-	}
-
-	cfg, err := config.Read(f)
-	if err != nil {
-		panic(err)
-	}
-
-	emoji.Printf(logOperating, cfg.Root)
-	for _, app := range cfg.Services {
-		if util.StringSliceContains(c.StringSlice("exclude"), app.Name) {
-			emoji.Printf(logSkipping, app.Name)
-			continue
-		}
-
-		emoji.Printf(logUpdating, app.Name)
-		git.Update(path.Join(cfg.Root, app.Name))
+		fmt.Print("\n\t", string(out))
 	}
 
 	return nil
